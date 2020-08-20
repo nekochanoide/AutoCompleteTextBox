2286573a27430978007dcbc7516cc533c03fb351
Matvey Kupriyanov <motya202@gmail.com>
2020-08-20 14:41:55 +0500

    Add project files.

---
+++ AutoCompleteTextBox/Window1.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace AutoCompleteTextBox
+{
+
+    public class Model : INotifyPropertyChanged
+    {
+        string _text;
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                _text = value; 
+                OnPropertyChanged("Text");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            DataContext = new Model();
+        }
+    }
+}
